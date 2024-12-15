09f26f76b2a673d36ba1ae7e4bdf2064a8e295b5
TaynCabreraOladeinde <116230722+TaynCabreraOladeinde@users.noreply.github.com>
2024-12-15 10:19:54 -0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+        // Instantiate your objects
+        User user = new User("JohnDoe");
+        //print your objects
+        System.out.println(user); // Print the user object
+    }
+}
