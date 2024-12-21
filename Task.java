bce2dbb30daa99983e7fc86463cda6e3d7a7e52c
TaynCabreraOladeinde <116230722+TaynCabreraOladeinde@users.noreply.github.com>
2024-12-20 16:10:03 -0800

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,4 +1,26 @@
-public class Task {
-    
-}
+import java.time.LocalDateTime;
 
+public class Task {
+    private String title;
+    private boolean isComplete;
+    private LocalDateTime dueDate;
+
+    public Task(String title, LocalDateTime dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.isComplete = false;
+    }
+
+    public void markAsComplete() {
+        this.isComplete = true;
+    }
+
+    public String getDetails() {
+        return "Title: " + title + ", Completed: " + isComplete + ", Due Date: " + dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return getDetails();
+    }
+}
