a7a8cf056877934ecce01034737e3d6dbed79c78
TaynCabreraOladeinde <116230722+TaynCabreraOladeinde@users.noreply.github.com>
2024-12-20 16:10:19 -0800

    Update Todolist.java

--- Todolist.java
+++ Todolist.java
@@ -1,4 +1,47 @@
-public class ToDoList {
-    
-}
+import java.util.ArrayList;
+import java.util.List;
 
+public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getPendingTasks() {
+        List<Task> pendingTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (!task.isComplete) {
+                pendingTasks.add(task);
+            }
+        }
+        return pendingTasks;
+    }
+
+    public List<Task> getCompletedTasks() {
+        List<Task> completedTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isComplete) {
+                completedTasks.add(task);
+            }
+        }
+        return completedTasks;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("ToDoList:\n");
+        for (Task task : tasks) {
+            sb.append(task.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
