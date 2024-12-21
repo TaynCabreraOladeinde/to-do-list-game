dd7d37bd398c6703849a483ce8d00c6f35f1b392
TaynCabreraOladeinde <116230722+TaynCabreraOladeinde@users.noreply.github.com>
2024-12-20 16:10:37 -0800

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,27 @@
 public class User {
-    
-}
+    private String username;
+    private ToDoList toDoList;
 
+    public User(String username) {
+        this.username = username;
+        this.toDoList = new ToDoList();
+    }
+
+    public void createTask(String title) {
+        Task newTask = new Task(title, null); // Assuming no due date for simplicity
+        toDoList.addTask(newTask);
+    }
+
+    public void deleteTask(Task task) {
+        toDoList.removeTask(task);
+    }
+
+    public void viewTasks() {
+        System.out.println(toDoList.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + username + "\n" + toDoList.toString();
+    }
+}
